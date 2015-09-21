0ab23b56092096b2a8ecf45d002ad7efdb5e871d
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-09-21 03:49:47 -0400

    moved files around and setup root directory

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,2 +1,51 @@
 class Clip < ActiveRecord::Base
+
+	
+	       ####  Copy  ####
+
+	def copy_1
+		variable1 = Clipboard.paste
+	end
+
+	def copy_2
+		variable2 = Clipboard.paste
+	end
+
+	def copy_3
+		variable3 = Clipboard.paste
+	end
+
+	def copy_4
+		variable4 = Clipboard.paste
+	end
+	def copy_5
+		variable5 = Clipboard.paste
+	end
+
+
+	      #### Pasta  ####
+
+	def pasta_1
+		Clipboard.copy(variable1)
+		Clipboard.paste
+	end
+
+	def pasta_2
+		Clipboard.copy(variable2)
+		Clipboard.paste
+	end
+	def pasta_3
+		Clipboard.copy(variable1)
+		Clipboard.paste
+	end
+
+	def pasta_4
+		Clipboard.copy(variable2)
+		Clipboard.paste
+	end
+	def pasta_5
+		Clipboard.copy(variable1)
+		Clipboard.paste
+	end
+	end
 end
